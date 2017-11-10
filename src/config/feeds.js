4a676d6fb702ea5f3f191293eda1cb48c25a5ae1
Francesco Leonardi <fleonardi86@gmail.com>
2017-11-10 11:46:22 +0000

    updated api params

--- src/config/feeds.js
+++ src/config/feeds.js
@@ -1,3 +1,3 @@
 export function googleApis(latitude, longitude, authToken) {
-  return 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=1400&type=restaurant&key=AIzaSyAzs4PE09t8owoKD5ZewNXsts6NqTViy2c';
+  return 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + latitude + ',' + longitude + '&radius=700&rankby=prominence&opennow=true&type=restaurant&key=AIzaSyAzs4PE09t8owoKD5ZewNXsts6NqTViy2c';
 }
